063a5ed68ea342f9545f580bd0f75401f13ecbef
github username <github email address>
2019-07-18 15:12:27 -0400

    Done.

--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,18 @@
+# Destroy.all
 class Ship
+  attr_accessor :name, :type, :booty
+  @@ships = []
+
+  def initialize(args)
+    @name = args[:name]
+    @type = args[:type]
+    @booty = args[:booty]
+    @@ships << self
+  end
+
+
+  def self.clear
+    @@ships = []
+  end
+
 end
